09be4a28acf06ff42fb861d1c7d105171fe5108f
Wesley Francescon <wfrancescons@gmail.com>
2026-04-23 00:34:53 -0300

    refactor: restructure site content and layout

--- _config.ts
+++ _config.ts
@@ -1,50 +1,13 @@
 import lume from "lume/mod.ts";
-import favicon from "lume/plugins/favicon.ts";
-import googleFonts from "lume/plugins/google_fonts.ts";
-import inline from "lume/plugins/inline.ts";
-import jsx from "lume/plugins/jsx.ts";
-import lightningCss from "lume/plugins/lightningcss.ts";
-import metas from "lume/plugins/metas.ts";
-import minifyHTML from "lume/plugins/minify_html.ts";
-import picture from "lume/plugins/picture.ts";
-import robots from "lume/plugins/robots.ts";
-import seo from "lume/plugins/seo.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
-import terser from "lume/plugins/terser.ts";
-import transformImages from "lume/plugins/transform_images.ts";
+import theme from "theme/mod.ts";
+
+const siteUrl = "https://wfrancescons.github.io/";
 
 const site = lume({
   src: "./src",
+  location: new URL(siteUrl),
 });
 
-site.use(jsx());
-site.use(terser());
-site.use(googleFonts({
-  fonts:
-    "https://fonts.google.com/share?selection.family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900",
-}));
-site.use(tailwindcss());
-site.use(lightningCss());
-site.use(picture());
-site.use(transformImages());
-site.use(metas());
-site.use(inline());
-site.use(favicon({
-  input: "/img/avatar.jpg",
-}));
-site.use(robots());
-site.use(seo({
-  options: { body: false },
-}));
-site.use(minifyHTML({
-  options: {
-    minify_js: true,
-    keep_html_and_head_opening_tags: true,
-  },
-}));
-
-site.add("img");
-site.add("style.css");
-site.add("assets", ".");
+site.use(theme());
 
 export default site;
